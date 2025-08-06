2694cb48ef7c3b870f7deeeaad85c836ee9970ef
hergam <djameledinee@gmail.com>
2025-08-06 12:15:41 -0700

    comment

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,8 +6,11 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
  
- class khaled (models.Model):
-    title = models.CharField(max_length=100)
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment on {self.post.title} by {self.created_at}'
  
